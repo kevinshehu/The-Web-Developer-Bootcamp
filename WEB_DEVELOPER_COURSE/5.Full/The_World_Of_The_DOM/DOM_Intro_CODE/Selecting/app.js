a85ad35e6e1a073d60cd26160846ef61b90736a4
Kevin Shehu <kshehu19@epoka.edu.al>
2021-10-01 15:57:37 +0200

    commit

---
+++ WEB_DEVELOPER_COURSE/5.Full/The_World_Of_The_DOM/DOM_Intro_CODE/Selecting/app.js
@@ -0,0 +1,29 @@
+// get all elements of name img
+// allImgs is a collection not an array
+const allImgs = document.getElementsByTagName('img');
+
+for (let temp of allImgs) {
+    console.log(temp.src);
+}
+
+// change all img soruces
+for (let temp of allImgs) {
+    temp.src = 'https://www.timeoutdubai.com/public/styles/full_img/public/images/2020/07/13/IMG-Dubai-UAE.jpg?itok=j4dmDDZa';
+}
+
+const linksInsidePharagraph = document.querySelectorAll('p>a');
+
+for (let temp of linksInsidePharagraph) {
+    console.log(temp.href);
+}
+
+// get text of the pharagraph
+const pharagraphText = document.querySelector('p').innerText;
+console.log(pharagraphText);
+
+// change every link in the page
+const allLinks = document.querySelectorAll('a');
+for (let temp of allLinks) {
+    temp.innerText = 'CLICK ME MF!';
+}
+
