4a16959cc535abe23cd5a7e86621ca4d16254db8
Kevin Shehu <kshehu19@epoka.edu.al>
2021-10-03 15:52:23 +0200

    commit

--- WEB_DEVELOPER_COURSE/5.Full/The_World_Of_The_DOM/Pokemon/app.js
+++ WEB_DEVELOPER_COURSE/5.Full/The_World_Of_The_DOM/Pokemon/app.js
@@ -1,19 +1,29 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
-const container = document.querySelector('#container');
-const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+// declare container
+const container = document.querySelector("#container");
 
+const baseUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
-for (let i = 1; i <= 151; i++) {
+// loop
+for (let i = 1; i < 200; i++) {
+    // create div element
     const pokemon = document.createElement('div');
-    pokemon.classList.add('pokemon');
+
+    // create span element
     const label = document.createElement('span');
     label.innerText = `#${i}`;
+
+    // create img element
     const newImg = document.createElement('img');
-    newImg.src = `${baseURL}${i}.png`
 
+    // give source
+    newImg.src = `${baseUrl}${i}.png`;
 
+    // append into div
     pokemon.appendChild(newImg);
     pokemon.appendChild(label);
-    container.appendChild(pokemon)
+
+    // append into container
+    container.appendChild(pokemon);
 }
